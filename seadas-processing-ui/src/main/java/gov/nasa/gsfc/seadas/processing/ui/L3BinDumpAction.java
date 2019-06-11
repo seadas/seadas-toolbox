55388f786881116472756652c272e8ac9f22b991
Aynur Abdurazik <aynur.abdurazik@nasa.gov>
2019-06-11 15:19:19 -0400

    Code cleanup

---
+++ seadas-processing-ui/src/main/java/gov/nasa/gsfc/seadas/processing/ui/L3BinDumpAction.java
@@ -0,0 +1,64 @@
+package gov.nasa.gsfc.seadas.processing.ui;
+
+import gov.nasa.gsfc.seadas.processing.common.CallCloProgramAction;
+import org.esa.snap.core.datamodel.ProductNode;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.*;
+
+import javax.swing.*;
+
+/**
+ * @author Aynur Abdurazik
+ * @since SeaDAS 8.0
+ * @see
+ */
+@ActionID(
+        category = "Processing", id = "gov.nasa.gsfc.seadas.processing.ui.L3BinDumpAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_ L3BinDumpAction_Name",
+        popupText = "#CTL_ L3BinDumpAction_Name"
+)
+@ActionReference(
+        path = "Menu/OC Processing",
+        position = 210
+)
+@NbBundle.Messages({
+        "CTL_L3BinDumpAction_Name=l3bindump...",
+        "CTL_L3BinDumpAction_ProgramName=l3bindump",
+        "CTL_L3BinDumpAction_DialogTitle=l3bindump",
+        "CTL_L3BinDumpAction_XMLFileName=l3bindump.xml",
+        "CTL_L3BinDumpAction_Description=Create a L3 Mapped Image."
+})
+
+public class L3BinDumpAction extends CallCloProgramAction implements ContextAwareAction, LookupListener {
+
+    private final Lookup lkp;
+
+    public L3BinDumpAction() {
+        this(Utilities.actionsGlobalContext());
+    }
+
+    public L3BinDumpAction(Lookup lkp) {
+        this.lkp = lkp;
+        Lookup.Result<ProductNode> lkpContext = lkp.lookupResult(ProductNode.class);
+        lkpContext.addLookupListener(WeakListeners.create(LookupListener.class, this, lkpContext));
+        putValue(Action.NAME, Bundle.CTL_L3BinDumpAction_Name());
+        putValue(Action.SHORT_DESCRIPTION, Bundle.CTL_L3BinDumpAction_Description());
+        setProgramName(Bundle.CTL_L3BinDumpAction_ProgramName());
+        setDialogTitle(Bundle.CTL_L3BinDumpAction_DialogTitle());
+        setXmlFileName(Bundle.CTL_L3BinDumpAction_XMLFileName());
+    }
+
+    @Override
+    public void resultChanged(LookupEvent lookupEvent) {
+
+    }
+
+    @Override
+    public Action createContextAwareInstance(Lookup actionContext) {
+        return new L3BinDumpAction(actionContext);
+    }
+}
---
+++ seadas-processing-ui/src/main/java/gov/nasa/gsfc/seadas/processing/ui/SmiGenAction.java
@@ -0,0 +1,64 @@
+package gov.nasa.gsfc.seadas.processing.ui;
+
+import gov.nasa.gsfc.seadas.processing.common.CallCloProgramAction;
+import org.esa.snap.core.datamodel.ProductNode;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.*;
+
+import javax.swing.*;
+
+/**
+ * @author Aynur Abdurazik
+ * @since SeaDAS 8.0
+ * @see
+ */
+@ActionID(
+        category = "Processing", id = "gov.nasa.gsfc.seadas.processing.ui.SmiGenAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_ SmiGenAction_Name",
+        popupText = "#CTL_ SmiGenAction_Name"
+)
+@ActionReference(
+        path = "Menu/OC Processing",
+        position = 200
+)
+@NbBundle.Messages({
+        "CTL_SmiGenAction_Name=smigen...",
+        "CTL_SmiGenAction_ProgramName=smigen",
+        "CTL_SmiGenAction_DialogTitle=smigen",
+        "CTL_SmiGenAction_XMLFileName=smigen.xml",
+        "CTL_SmiGenAction_Description=Create a L3 map file."
+})
+
+public class SmiGenAction extends CallCloProgramAction implements ContextAwareAction, LookupListener {
+
+    private final Lookup lkp;
+
+    public  SmiGenAction() {
+        this(Utilities.actionsGlobalContext());
+    }
+
+    public   SmiGenAction(Lookup lkp) {
+        this.lkp = lkp;
+        Lookup.Result<ProductNode> lkpContext = lkp.lookupResult(ProductNode.class);
+        lkpContext.addLookupListener(WeakListeners.create(LookupListener.class, this, lkpContext));
+        putValue(Action.NAME, Bundle.CTL_SmiGenAction_Name());
+        putValue(Action.SHORT_DESCRIPTION, Bundle.CTL_SmiGenAction_Description());
+        setProgramName(Bundle.CTL_SmiGenAction_ProgramName());
+        setDialogTitle(Bundle.CTL_SmiGenAction_DialogTitle());
+        setXmlFileName(Bundle.CTL_SmiGenAction_XMLFileName());
+    }
+
+    @Override
+    public void resultChanged(LookupEvent lookupEvent) {
+
+    }
+
+    @Override
+    public Action createContextAwareInstance(Lookup actionContext) {
+        return new SmiGenAction(actionContext);
+    }
+}
