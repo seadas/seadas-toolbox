794d15624803e1e117079e4b55fbbfccf0fbdfce
Aynur Abdurazik <aynur.abdurazik@nasa.gov>
2019-06-21 14:44:10 -0400

    Code cleanup

---
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/ui/L2BinAquariusAction.java
@@ -0,0 +1,70 @@
+package gov.nasa.gsfc.seadas.processing.ui;
+
+import gov.nasa.gsfc.seadas.processing.common.CallCloProgramAction;
+import org.esa.snap.core.datamodel.ProductNode;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.*;
+
+import javax.swing.*;
+
+/**
+ * @author Aynur Abdurazik
+ * @since SeaDAS 8.0
+ * @see
+ */
+@ActionID(
+        category = "Processing", id = "gov.nasa.gsfc.seadas.processing.ui.L2BinAquariusAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_ L2BinAquariusAction_Name",
+        popupText = "#CTL_ L2BinAquariusAction_Name"
+)
+@ActionReference(
+        path = "Menu/OC Processing",
+        position = 160
+)
+@NbBundle.Messages({
+        "CTL_L2BinAquariusAction_Name=l2bin_aquarius...",
+        "CTL_L2BinAquariusAction_ProgramName=l2bin_aquarius",
+        "CTL_L2BinAquariusAction_DialogTitle=l2bin_aquarius",
+        "CTL_L2BinAquariusAction_XMLFileName=l2bin_aquarius.xml",
+        "CTL_L2BinAquariusAction_Description=Create a L3 bin file from Aquarius L2 file(s)."
+})
+
+
+public class L2BinAquariusAction extends CallCloProgramAction implements ContextAwareAction, LookupListener {
+
+    private final Lookup lkp;
+
+    public  L2BinAquariusAction() {
+        this(Utilities.actionsGlobalContext());
+    }
+
+    public   L2BinAquariusAction(Lookup lkp) {
+        this.lkp = lkp;
+        Lookup.Result<ProductNode> lkpContext = lkp.lookupResult(ProductNode.class);
+        lkpContext.addLookupListener(WeakListeners.create(LookupListener.class, this, lkpContext));
+        putValue(Action.NAME, Bundle.CTL_L2BinAquariusAction_Name());
+        putValue(Action.SHORT_DESCRIPTION, Bundle.CTL_L2BinAquariusAction_Description());
+        setProgramName(Bundle.CTL_L2BinAquariusAction_ProgramName());
+        setDialogTitle(Bundle.CTL_L2BinAquariusAction_DialogTitle());
+        setXmlFileName(Bundle.CTL_L2BinAquariusAction_XMLFileName());
+    }
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//
+//    }
+
+    @Override
+    public void resultChanged(LookupEvent lookupEvent) {
+
+    }
+
+    @Override
+    public Action createContextAwareInstance(Lookup actionContext) {
+        return new L2BinAquariusAction(actionContext);
+    }
+}
---
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/ui/L2GenSnapAction.java
@@ -0,0 +1,77 @@
+package gov.nasa.gsfc.seadas.processing.ui;
+
+import gov.nasa.gsfc.seadas.processing.l2gen.userInterface.L2genAction;
+import org.esa.snap.core.datamodel.ProductNode;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.*;
+
+import javax.swing.*;
+
+/**
+ * @author Aynur Abdurazik
+ * @since SeaDAS 8.0
+ * @see
+ */
+//
+@ActionID(
+        category = "Processing", id = "gov.nasa.gsfc.seadas.processing.ui.L2GenSnapAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_ L2GenAction_Name",
+        popupText = "#CTL_ L2GenAction_Name"
+)
+@ActionReference(
+        path = "Menu/OC Processing",
+        position = 110
+)
+@NbBundle.Messages({
+        "CTL_L2GenAction_Name=l2gen...",
+        "CTL_L2GenAction_ProgramName=l2gen",
+        "CTL_L2GenAction_DialogTitle=l2gen",
+        "CTL_L2GenAction_XMLFileName=l2gen.xml",
+        "CTL_L2GenAction_Description=Process a L1 file to L2."
+})
+
+public class L2GenSnapAction extends L2genAction implements ContextAwareAction, LookupListener {
+
+    private final Lookup lkp;
+
+    public L2GenSnapAction() {
+        this(Utilities.actionsGlobalContext());
+    }
+
+    /**
+     *
+     * @param lkp
+     */
+    public L2GenSnapAction(Lookup lkp) {
+        this.lkp = lkp;
+        Lookup.Result<ProductNode> lkpContext = lkp.lookupResult(ProductNode.class);
+        lkpContext.addLookupListener(WeakListeners.create(LookupListener.class, this, lkpContext));
+        putValue(Action.NAME, Bundle.CTL_L2GenAction_Name());
+        putValue(Action.SHORT_DESCRIPTION, Bundle.CTL_L2GenAction_Description());
+        setProgramName(Bundle.CTL_L2GenAction_ProgramName());
+        setDialogTitle(Bundle.CTL_L2GenAction_DialogTitle());
+        setXmlFileName(Bundle.CTL_L2GenAction_XMLFileName());
+    }
+    /**
+     * @param lookupEvent
+     * @
+     */
+    @Override
+    public void resultChanged(LookupEvent lookupEvent) {
+
+    }
+
+    /**
+     *
+     * @param actionContext
+     * @return
+     */
+    @Override
+    public Action createContextAwareInstance(Lookup actionContext) {
+        return new L2GenSnapAction(actionContext);
+    }
+}
