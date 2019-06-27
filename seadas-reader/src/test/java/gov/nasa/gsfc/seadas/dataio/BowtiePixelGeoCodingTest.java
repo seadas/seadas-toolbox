ac30399fa7c6d9f0debdc1088706b4b9a5b2cf80
Aynur Abdurazik <aynur.abdurazik@nasa.gov>
2019-06-27 14:24:04 -0400

    Updated with SNAP dependencies.

--- seadas-reader/src/test/java/gov/nasa/gsfc/seadas/dataio/BowtiePixelGeoCodingTest.java
+++ seadas-reader/src/test/java/gov/nasa/gsfc/seadas/dataio/BowtiePixelGeoCodingTest.java
@@ -1,35 +1,45 @@
 package gov.nasa.gsfc.seadas.dataio;
 
+import org.esa.snap.core.dataio.ProductFlipper;
+import org.esa.snap.core.dataio.ProductIO;
+import org.esa.snap.core.dataio.ProductSubsetBuilder;
+import org.esa.snap.core.dataio.ProductSubsetDef;
+import org.esa.snap.core.datamodel.GeoCoding;
+import org.esa.snap.core.datamodel.GeoPos;
+import org.esa.snap.core.datamodel.PixelPos;
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.util.ProductUtils;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import org.esa.snap.core.dataio.ProductIO;
-import org.esa.snap.core.datamodel.Product;
 
-import static org.junit.Assert.*;
+import static org.esa.snap.core.util.Debug.assertNotNull;
+import static org.esa.snap.core.util.Debug.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class BowtiePixelGeoCodingTest {
 
     @Test
     public void testTransferGeoCoding() throws URISyntaxException, IOException {
         Product product = ProductIO.readProduct(new File(getClass().getResource("bowtiepixelgeocoding_test_product.L2_sub").toURI()));
-        assertTrue(product.getGeoCoding() instanceof BowtiePixelGeoCoding);
+        assertTrue(product.getSceneGeoCoding() instanceof BowtiePixelGeoCoding);
 
         Product targetProduct = new Product("name", "type", product.getSceneRasterWidth(), product.getSceneRasterHeight());
 
-        assertNull(targetProduct.getGeoCoding());
+        assertNull(targetProduct.getSceneGeoCoding());
         ProductUtils.copyGeoCoding(product, targetProduct);
 
-        assertNotNull(targetProduct.getGeoCoding());
-        assertTrue(targetProduct.getGeoCoding() instanceof BowtiePixelGeoCoding);
+        assertNotNull(targetProduct.getSceneGeoCoding());
+        assertTrue(targetProduct.getSceneGeoCoding() instanceof BowtiePixelGeoCoding);
     }
 
     @Test
     public void testLatAndLonAreCorrectlySubsetted() throws URISyntaxException, IOException {
         Product product = ProductIO.readProduct(new File(getClass().getResource("bowtiepixelgeocoding_test_product.L2_sub").toURI()));
-        GeoCoding sourcceGeoCoding = product.getGeoCoding();
+        GeoCoding sourcceGeoCoding = product.getSceneGeoCoding();
         assertTrue(sourcceGeoCoding instanceof BowtiePixelGeoCoding);
 
         ProductSubsetDef subsetDef = new ProductSubsetDef();
@@ -37,16 +47,16 @@ public class BowtiePixelGeoCodingTest {
         subsetDef.addNodeName("chlor_a");
         Product targetProduct = product.createSubset(subsetDef, "subset", "");
 
-        GeoCoding targetGeoCoding = targetProduct.getGeoCoding();
-        assertNotNull(targetGeoCoding);
-        assertTrue(targetGeoCoding instanceof BowtiePixelGeoCoding);
+        GeoCoding targetSceneGeoCoding = targetProduct.getSceneGeoCoding();
+        assertNotNull(targetSceneGeoCoding);
+        assertTrue(targetSceneGeoCoding instanceof BowtiePixelGeoCoding);
         assertTrue(targetProduct.containsBand("latitude"));
         assertTrue(targetProduct.containsBand("longitude"));
 
         PixelPos sourcePixelPos = new PixelPos(50.5f, 50.5f);
         GeoPos expected = sourcceGeoCoding.getGeoPos(sourcePixelPos, new GeoPos());
         PixelPos targetPixelPos = new PixelPos(0.5f, 0.5f);
-        GeoPos actual = targetGeoCoding.getGeoPos(targetPixelPos, new GeoPos());
+        GeoPos actual = targetSceneGeoCoding.getGeoPos(targetPixelPos, new GeoPos());
         assertEquals(expected.getLat(), actual.getLat(), 1.0e-6);
         assertEquals(expected.getLon(), actual.getLon(), 1.0e-6);
     }
@@ -57,12 +67,12 @@ public class BowtiePixelGeoCodingTest {
         Product product = ProductIO.readProduct(new File(getClass().getResource("bowtiepixelgeocoding_test_product.L2_sub").toURI()));
 
         // latitude values increasing
-        BowtiePixelGeoCoding bowtiePixelGeoCoding = (BowtiePixelGeoCoding) product.getGeoCoding();
+        BowtiePixelGeoCoding bowtiePixelGeoCoding = (BowtiePixelGeoCoding) product.getSceneGeoCoding();
         assertEquals(0, bowtiePixelGeoCoding.getScanlineOffset());
 
         // flipped product, latitude values decreasing
         Product flippedProduct = ProductFlipper.createFlippedProduct(product, ProductFlipper.FLIP_BOTH, "f", "f");
-        bowtiePixelGeoCoding = (BowtiePixelGeoCoding) flippedProduct.getGeoCoding();
+        bowtiePixelGeoCoding = (BowtiePixelGeoCoding) flippedProduct.getSceneGeoCoding();
         assertEquals(0, bowtiePixelGeoCoding.getScanlineOffset());
 
         // small product, just one scan (10 lines)
@@ -85,7 +95,7 @@ public class BowtiePixelGeoCodingTest {
         ProductSubsetDef subsetDef = new ProductSubsetDef();
         subsetDef.setRegion(0, yStart, product.getSceneRasterWidth(), heigth);
         Product subsetProduct = ProductSubsetBuilder.createProductSubset(product, subsetDef, "s", "s");
-        BowtiePixelGeoCoding bowtiePixelGeoCoding = (BowtiePixelGeoCoding) subsetProduct.getGeoCoding();
+        BowtiePixelGeoCoding bowtiePixelGeoCoding = (BowtiePixelGeoCoding) subsetProduct.getSceneGeoCoding();
         assertEquals("for y=" + yStart + " scanlineOffset", scanlineOffset, bowtiePixelGeoCoding.getScanlineOffset());
     }
 }
