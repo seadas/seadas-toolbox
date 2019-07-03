d288331b44f81731761b350464136fb9db10dbb7
Default Seadas User <seadas-user@localhost>
2019-07-03 12:30:38 -0400

    Made install directory uneditable

--- seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/common/OCSSWInstallerForm.java
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/common/OCSSWInstallerForm.java
@@ -228,14 +228,17 @@ public abstract class OCSSWInstallerForm extends JPanel implements CloProgramUI 
                 if (! ocsswInfo.getOcsswLocation().equals(ocsswInfo.OCSSW_LOCATION_LOCAL)) {
                     //if ocssw is not local, then disable the button to choose ocssw installation directory
                     ((JLabel)dirPanel.getComponent(0)).setText("Remote install-dir");
-                    dirPanel.getComponent(1).setEnabled(false);
-                    dirPanel.getComponent(2).setEnabled(false);
-                    //dirPanel.getComponent(2).setVisible(false);
+                    ((JTextField)dirPanel.getComponent(1)).setEditable(false);
+//                    dirPanel.getComponent(2).setEnabled(false);
+                    dirPanel.getComponent(2).setVisible(false);
                 } else {
                     ((JLabel)dirPanel.getComponent(0)).setText("Local install-dir");
                 }
-                dirPanel.getComponent(1).setEnabled(false);
-                dirPanel.getComponent(2).setEnabled(false);
+                ((JTextField)dirPanel.getComponent(1)).setEditable(false);
+//                dirPanel.getComponent(1).setEnabled(false);
+//                dirPanel.getComponent(2).setEnabled(false);
+                dirPanel.getComponent(2).setVisible(false);
+
             }
         }
 
