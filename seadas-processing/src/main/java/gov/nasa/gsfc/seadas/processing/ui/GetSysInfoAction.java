ba2ae3dafabf2223f7c58a8df1a4de3996aa60d0
xuanyang02 <xunyang02@yahoo.com>
2020-06-10 16:23:26 -0400

    Made preliminary GUI to get system and OCSSW info for trouble shooting forum

---
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/ui/GetSysInfoAction.java
@@ -0,0 +1,77 @@
+package gov.nasa.gsfc.seadas.processing.ui;
+
+import gov.nasa.gsfc.seadas.processing.ocssw.GetSysInfoGUI;
+import org.esa.snap.core.datamodel.ProductNode;
+import org.esa.snap.rcp.actions.AbstractSnapAction;
+import org.esa.snap.ui.AppContext;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * @author Bing Yang
+ * @since SeaDAS 8.0
+ * @see
+ */
+@ActionID(
+        category = "Processing", id = "gov.nasa.gsfc.seadas.processing.ui.GetSysInfoAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_ GetSysInfoAction_Name",
+        popupText = "#CTL_ GetSysInfoAction_Name"
+)
+@ActionReference(
+        path = "Menu/SeaDAS-OCSSW",
+        position = 260
+)
+@NbBundle.Messages({
+        "CTL_GetSysInfoAction_Name=getSysInfo...",
+        "CTL_GetSysInfoAction_DialogTitle=get_sys_info",
+        "CTL_GetSysInfoAction_Description=Print SeaDAS and OCSSW info for trouble shooting."
+})
+public class GetSysInfoAction extends AbstractSnapAction implements ContextAwareAction, LookupListener {
+
+    private final Lookup lkp;
+
+    public GetSysInfoAction() {
+        this(Utilities.actionsGlobalContext());
+    }
+
+    public GetSysInfoAction(Lookup lkp) {
+        this.lkp = lkp;
+        Lookup.Result<ProductNode> lkpContext = lkp.lookupResult(ProductNode.class);
+        lkpContext.addLookupListener(WeakListeners.create(LookupListener.class, this, lkpContext));
+        putValue(Action.NAME, Bundle.CTL_GetSysInfoAction_Name());
+        putValue(Action.SHORT_DESCRIPTION, Bundle.CTL_GetSysInfoAction_Description());
+    }
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//
+//    }
+
+    @Override
+    public void resultChanged(LookupEvent lookupEvent) {
+
+    }
+
+    @Override
+    public Action createContextAwareInstance(Lookup actionContext) {
+        return new GetSysInfoAction(actionContext);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        final AppContext appContext = getAppContext();
+
+        final Window parent = appContext.getApplicationWindow();
+        GetSysInfoGUI getSysInfoGUI = new GetSysInfoGUI();
+        getSysInfoGUI.init(parent);
+    }
+}
