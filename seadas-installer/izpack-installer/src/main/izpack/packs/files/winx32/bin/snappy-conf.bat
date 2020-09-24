cd6b0d95ea0af597e8ec8fd124091b7e44b22864
aynur <aynur.abdurazik@gmail.com>
2020-09-24 11:15:13 -0400

    adding the "packs" directory.

---
+++ seadas-installer/izpack-installer/src/main/izpack/packs/files/winx32/bin/snappy-conf.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if [%1] == [] goto Usage
+if [%1] == [/?] goto Usage
+if not [%3] == [] goto Usage
+
+"%~dp0\snap.exe" --nogui --nosplash --python %1 %2
+goto End
+
+:Usage
+@echo Configures the SNAP-Python interface 'snappy'.
+@echo. 
+@echo %~n0 Python [Dir] ^| [/?]
+@echo.  
+@echo     Python: Full path to Python executable to be used with SNAP, e.g. C:\Python34\python.exe
+@echo     Dir:    Directory where the 'snappy' module should be installed. Defaults to %USERPROFILE%\.snap\snap-python
+@echo.
+:End
