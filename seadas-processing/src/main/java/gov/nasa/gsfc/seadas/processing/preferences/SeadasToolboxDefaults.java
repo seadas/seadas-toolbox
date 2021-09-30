bd9aa48c081318fd345230114a694b86b4c4b33b
Daniel Knowles <daniel.s.knowles@nasa.gov>
2021-09-30 15:23:17 -0400

    L2 gen: modified to use shortcuts as default

--- seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/preferences/SeadasToolboxDefaults.java
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/preferences/SeadasToolboxDefaults.java
@@ -15,7 +15,7 @@ public class SeadasToolboxDefaults {
     public static final String PROPERTY_L2GEN_SHORTCUTS_KEY = PROPERTY_L2GEN_ROOT_KEY + ".shortcuts";
     public static final String PROPERTY_L2GEN_SHORTCUTS_LABEL = "L2prod Wavelength Shortcuts";
     public static final String PROPERTY_L2GEN_SHORTCUTS_TOOLTIP = "Use wavelength shortcuts (i.e. Rrs_vvv, Rrs_iii) in l2prod";
-    public static final boolean PROPERTY_L2GEN_SHORTCUTS_DEFAULT = false;
+    public static final boolean PROPERTY_L2GEN_SHORTCUTS_DEFAULT = true;
     public static final Class PROPERTY_L2GEN_SHORTCUTS_TYPE = Boolean.class;
 
 
