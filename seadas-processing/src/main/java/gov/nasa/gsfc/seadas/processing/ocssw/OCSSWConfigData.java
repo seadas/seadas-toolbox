bb8f05b200fa9dd7db08197b5606ca485397cac4
Daniel Knowles <daniel.s.knowles@nasa.gov>
2023-05-24 12:40:04 -0400

    Updated default OCSSW tag to V2022.3

--- seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/ocssw/OCSSWConfigData.java
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/ocssw/OCSSWConfigData.java
@@ -61,7 +61,7 @@ public class OCSSWConfigData {
     //public final static String SEADAS_CLIENT_SERVER_SHARED_DIR_DEFAULT_VALUE =  System.getProperty("user.home") + File.separator + "seadasClientServerShared";
     final static String SEADAS_CLIENT_SERVER_SHARED_DIR_NAME = "seadasClientServerShared";
     final static String SEADAS_OCSSW_DEBUG_DEFAULT_VALUE =  "false";
-    public final static String SEADAS_OCSSW_TAG_DEFAULT_VALUE = "V2022.0";
+    public final static String SEADAS_OCSSW_TAG_DEFAULT_VALUE = "V2022.3";
 
 
     final static String OCSSW_TAG_LABEL = "Valid OCSSW Tags";
