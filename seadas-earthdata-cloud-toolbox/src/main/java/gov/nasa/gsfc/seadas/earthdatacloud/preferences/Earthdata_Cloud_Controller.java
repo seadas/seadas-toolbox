8650755ec04ad0575062b45a29f19057c985b3d5
Daniel Knowles <daniel.s.knowles@nasa.gov>
2025-05-07 14:57:08 -0400

    Updated Versioning to 2.2.1-RC1 and updated OB_CLOUD Data Browser user_regions file

--- seadas-earthdata-cloud-toolbox/src/main/java/gov/nasa/gsfc/seadas/earthdatacloud/preferences/Earthdata_Cloud_Controller.java
+++ seadas-earthdata-cloud-toolbox/src/main/java/gov/nasa/gsfc/seadas/earthdatacloud/preferences/Earthdata_Cloud_Controller.java
@@ -125,7 +125,7 @@ public final class Earthdata_Cloud_Controller extends DefaultConfigController {
     public static final String PROPERTY_USER_REGION_INCLUDE_KEY = PROPERTY_ROOT_KEY + ".region.selector";
     public static final String PROPERTY_USER_REGION_INCLUDE_LABEL = "User Region Selector";
     public static final String PROPERTY_USER_REGION_INCLUDE_TOOLTIP = "Include 'user_region' selector in GUI";
-    public static final boolean PROPERTY_USER_REGION_INCLUDE_DEFAULT = true;
+    public static final boolean PROPERTY_USER_REGION_INCLUDE_DEFAULT = false;
     
     
     public static final String PROPERTY_DAYNIGHT_MODE_KEY = PROPERTY_ROOT_KEY + ".daynight_mode";
