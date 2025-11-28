3f35c037793f7c2d69f68fa159f14cd631ba7b7d
Daniel Knowles <daniel.s.knowles@nasa.gov>
2025-11-28 10:29:29 -0500

    Modified layout to better fit as a SNAP plugin

--- seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/help/MenuLocationUtils.java
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/help/MenuLocationUtils.java
@@ -3,7 +3,7 @@ package gov.nasa.gsfc.seadas.processing.help;
 
 
 public class MenuLocationUtils {
-    public static final String SEADAS_VIDEOS_LOCATION = "Menu/Video-Tutorials";
-//    public static final String SEADAS_VIDEOS_LOCATION = "Menu/Help/SeaDAS/Video-Tutorials";
+//    public static final String SEADAS_VIDEOS_LOCATION = "Menu/Video-Tutorials";
+    public static final String SEADAS_VIDEOS_LOCATION = "Menu/Help/SeaDAS/Video-Tutorials";
 
 }
